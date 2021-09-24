62db82e8cc7d0483b5cdc5eda095f0b35423e0a4
João Gabriel <gsantana152@gmail.com>
2021-09-24 16:30:14 -0300

    Module 3 - Class 2 - Adding http client

--- lib/network/webclient.dart
+++ lib/network/webclient.dart
@@ -1,7 +1,29 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http/interceptor_contract.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
-void findAll() async {
+Future<void> findAll() async {
+  Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
   final Uri url = Uri(host: '192.168.0.114:8080/transactions');
   final Response response = await get(url);
-  print(response.body);
+}
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('Request');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
 }
