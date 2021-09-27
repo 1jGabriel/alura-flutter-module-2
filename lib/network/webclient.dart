93efc3e615989901ebeb25919e8f5eb8401ef4cd
João Gabriel <gsantana152@gmail.com>
2021-09-27 15:56:22 -0300

    Module 4 - Class 3: - Refactoring code - Created http exception to handle timeout errors

--- lib/network/webclient.dart
+++ lib/network/webclient.dart
@@ -1,10 +1,9 @@
-
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 import 'interceptors/logging_interceptor.dart';
 
-final Client client =
-    InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()], requestTimeout: Duration(seconds: 5));
 
 final Uri baseUrl = Uri(host: '192.168.0.114:8080/transactions');
