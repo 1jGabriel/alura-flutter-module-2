c61343b62360ec36f9d5a3ed80c6180faea3130d
João Gabriel <gsantana152@gmail.com>
2021-09-26 23:24:05 -0300

    Module 4 - Class 4: - Added function to save transaction - Extracted client and baseUrl

--- lib/network/webclient.dart
+++ lib/network/webclient.dart
@@ -6,10 +6,14 @@ import 'package:http/http.dart';
 import 'package:http_interceptor/http/interceptor_contract.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
+final Client client =
+    InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+
+final Uri baseUrl = Uri(host: '192.168.0.114:8080/transactions');
+
 Future<List<Transaction>> findAll() async {
-  Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
-  final Uri url = Uri(host: '192.168.0.114:8080/transactions');
-  final Response response = await client.get(url).timeout(Duration(seconds: 5));
+
+  final Response response = await client.get(baseUrl).timeout(Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = [];
   for (Map<String, dynamic> transactionJson in decodedJson) {
@@ -25,6 +29,33 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value' : transaction.value,
+    'contact' : {
+      'name' : transaction.contact.name,
+      'accountNumber' : transaction.contact.accountNumber
+    }
+  };
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(baseUrl, headers: {
+    'Content-type': 'application/json',
+    'password': '1000',
+  }, body: transactionJson);
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transaction(
+    json['value'],
+    ContactModel(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
 }
 
 class LoggingInterceptor implements InterceptorContract {
